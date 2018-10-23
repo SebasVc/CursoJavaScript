89b0fcda6aafbd0c19812aa68c327f8e91a1e795
SebasVc <44382706+SebasVc@users.noreply.github.com>
2018-10-23 16:36:51 -0500

    Add files via upload

--- js/bom.js
+++ js/bom.js
@@ -1,9 +1,11 @@
 "use strict"
 /* BOM - Browser Object Model.
 permite usar  y editar las propiedades de un navegador web */
+	var Anchura= screen.width;
+	var Altura=screen.height;
 function getBom(){
-console.log("Anchura: "+screen.width+"px");
-console.log("Altura: "+screen.height+"px");
+console.log("Anchura: "+Anchura+"px");
+console.log("Altura: "+Altura+"px");
 console.log("Ruta: "+window.location);
 /*Con esto se muestra las propiedades sin importar la consola 
 console.log("Anchura: "+screen.width+"px");
@@ -12,3 +14,6 @@ console.log("Altura: "+screen.height+"px");*/
 function cambioURL(url){
 	window.location.href= url;
 }
+function abrirVentana(url){
+	window.open(url,"","fullscreen=yes, scrollbars=auto");
+	}
